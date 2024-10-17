0c1c7cd0d75c4ac365aa619ac84da335d2b7a490
Yash646398 <166711274+Yash646398@users.noreply.github.com>
2024-10-17 10:40:24 +0530

    8_single level inheritance.cpp

--- 8_single level inheritance.cpp
+++ 8_single level inheritance.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+using namespace std;
 
+// Base Class
+class Base {
+public:
+    Base() {
+        cout << "Base class constructor called!" << endl;
+    }
+};
+
+// Derived Class
+class Derived : public Base {
+public:
+    Derived() {
+        cout << "Derived class constructor called!" << endl;
+    }
+};
+
+int main() {
+    cout << "Creating object of Derived class:" << endl;
+    Derived d;  // This will call both constructors
+
+    return 0;
+}
